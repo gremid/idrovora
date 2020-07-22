e6b9cd9b92958d0ebbb08892afc3c347e96316de
Gregor Middell <gregor@middell.net>
2020-07-22 12:12:14 +0200

    Set up asynchronous HTTP handling

--- src/idrovora/http.clj
+++ src/idrovora/http.clj
@@ -1,34 +1,56 @@
 (ns idrovora.http
-  (:require [reitit.ring :as ring]
-            [reitit.coercion.spec]
-            [reitit.ring.coercion :as coercion]
-            [reitit.ring.middleware.muuntaja :as muuntaja]
-            [reitit.ring.middleware.exception :as exception]
-            [reitit.ring.middleware.parameters :as parameters]
-            [reitit.ring.spec :as spec]
+  (:require [clojure.spec.alpha :as s]
+            [clojure.tools.logging :as log]
+            [mount.core :as mount :refer [defstate]]
             [muuntaja.core :as m]
-            [mount.core :refer [defstate]]
-            [clojure.java.io :as io]
-            [mount.core :as mount]
-            [clojure.tools.logging :as log]))
+            [reitit.coercion.spec :refer [coercion]]
+            [reitit.ring :as ring]
+            [reitit.ring.coercion :as coercion]
+            [reitit.ring.middleware.exception :as exception]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [ring.middleware.defaults :as defaults]))
+
+(def exception-middleware
+  (exception/create-exception-middleware
+   (merge
+    exception/default-handlers
+    {::exception/wrap (fn [handler ^Throwable e request]
+                        (if-not (some-> e ex-data :type #{::ring/response})
+                          (log/warn e (.getMessage e)))
+                        (handler e request))})))
+
+(def middleware
+  [{:name ::defaults
+    :wrap #(defaults/wrap-defaults
+            % (-> defaults/secure-site-defaults
+                  (assoc-in [:proxy] true)
+                  (assoc-in [:session] false)
+                  (assoc-in [:cookies] false)
+                  (assoc-in [:security :ssl-redirect] false)))}
+   muuntaja/format-negotiate-middleware
+   muuntaja/format-response-middleware
+   exception-middleware
+   muuntaja/format-request-middleware
+   coercion/coerce-response-middleware
+   coercion/coerce-request-middleware])
 
 (defn create-handler
   [context-path]
   (ring/ring-handler
    (ring/router
     [context-path
-     {:coercion reitit.coercion.spec/coercion
+     {:coercion coercion
       :muuntaja m/instance
-      :middleware [parameters/parameters-middleware
-                   muuntaja/format-negotiate-middleware
-                   muuntaja/format-response-middleware
-                   exception/exception-middleware
-                   muuntaja/format-request-middleware
-                   coercion/coerce-response-middleware
-                   coercion/coerce-request-middleware]}
-     ["/status" (fn [_] {:status 200 :body {:status "ok"}})]])
-    (ring/routes
-     (ring/create-default-handler))))
+      :middleware middleware}
+     ["/:pipeline/:job/*path"
+      {:handler
+       (fn [req respond _]
+         (respond {:status 200 :body (get-in req [:parameters :path] {})}))
+       :parameters
+       {:path (s/keys :req-un [::pipeline ::job ::path])}}]])
+   (ring/routes
+    (ring/redirect-trailing-slash-handler)
+    (ring/create-default-handler))))
 
 (defstate server
   :start
@@ -38,7 +60,7 @@
     (require 'ring.adapter.jetty)
     ((ns-resolve 'ring.adapter.jetty 'run-jetty)
      (create-handler http-context-path)
-     {:port http-port :join? false}))
+     {:port http-port :join? false :async? true}))
   :stop
   (do
     (log/infof "Stopping HTTP server")
