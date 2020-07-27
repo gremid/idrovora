ee1c2382c89d32eb6c064b378fa562dbcc7c5793
Gregor Middell <gregor@middell.net>
2020-07-27 21:56:39 +0200

    Implements synchronous and asynchronous job execution

--- src/idrovora/workspace.clj
+++ src/idrovora/workspace.clj
@@ -10,91 +10,160 @@
             [mount.core :as mount :refer [defstate]]
             [ring.util.response :as response])
   (:import com.cronutils.model.Cron
-           java.io.File))
+           java.io.File
+           java.nio.file.NoSuchFileException))
+
+;; ## Canonical file and directory locations in the workspace.
 
 (defn ^File xpl-dir
   []
   (-> (mount/args) ::xpl-dir fs/file))
 
+(defn ^File pipeline-xpl
+  [pipeline]
+  (fs/file (xpl-dir) (str pipeline ".xpl")))
+
 (defn ^File job-dir
-  []
-  (-> (mount/args) ::job-dir fs/file))
+  ([] (-> (mount/args) ::job-dir fs/file))
+  ([pipeline job] (fs/file (job-dir) pipeline job)))
 
-(defn fs-event->job
-  [{:keys [file action] :as evt}]
-  (let [^File job-dir (job-dir)]
-    (if (fs/ancestor? job-dir file)
-      (let [[pipeline job p1 p2] (map str (fs/relativize-path job-dir file))]
-        (if (and pipeline job p2 (= "status" p1))
-          (let [^File xpl (fs/file (xpl-dir) (str pipeline ".xpl"))]
-            (if (.isFile xpl)
-              {:pipeline pipeline
-               :job job
-               :xpl xpl
-               :status (-> p2 str keyword)})))))))
+(defn ^File job-status-txt
+  [pipeline job status]
+  (fs/file (job-dir pipeline job) "status" (str status ".txt")))
 
-(defstate fs-events
-  :start (let [^File job-dir (fs/make-dir! (job-dir))
-               ch (a/chan)
-               on-event (fn [evt] (a/>!! ch (merge evt (fs-event->job evt))))
-               watcher (watch-dir on-event job-dir)]
-           (log/infof "Start watching '%s'" job-dir)
-           [ch watcher])
-  :stop (let [[ch watcher] fs-events]
-          (log/infof "Stop watching '%s'" (job-dir))
-          (close-watcher watcher)
-          (a/close! ch)))
-
-(def running-jobs
-  (atom {}))
+;; ## Job execution and status management
 
 (defn run-job!
-  [{:keys [pipeline job xpl]}]
-  (let [job-dir (fs/file (job-dir) pipeline job)
-        status (fs/file job-dir "status")
-        result-ready (fs/file status "result-ready")
-        job-failed (fs/file status "job-failed")]
+  [[pipeline job :as job-id]]
+  (let [xpl (pipeline-xpl pipeline)
+        job-dir (job-dir pipeline job)
+        result-ready (job-status-txt pipeline job "result-ready")
+        job-failed (job-status-txt pipeline job "job-failed")]
     (try
-      (log/debugf "Running %s" [pipeline job])
+      (log/debugf "Running %s" job-id)
       (xproc/run-pipeline!
        (fs/uri xpl)
        {"source-dir" (fs/uri job-dir "source")
         "result-dir" (fs/uri job-dir "result")})
       (fs/delete! job-failed true)
       (spit result-ready "")
-      (log/debugf "Completed %s" [pipeline job])
+      (log/debugf "Completed %s" job-id)
+      [pipeline job :completed]
       (catch Throwable t
-        (log/warnf t "Error running %s" [pipeline job])
         (fs/delete! result-ready true)
-        (spit job-failed "")))))
+        (spit job-failed "")
+        (log/warnf t "Error running %s" job-id)
+        [pipeline job :failed t]))))
 
-(defstate event-listener
-  :start (a/go-loop []
-           (let [[ch] fs-events]
-             (when-let [evt (a/<! ch)]
-               (condp = (evt :status)
-                 :source-ready (run-job! evt)
-                 (log/trace evt))
-               (recur)))))
+(def running-jobs
+  (atom {}))
+
+(defn remove-running-jobs
+  [rf]
+  (fn
+    ([] (rf))
+    ([result] (rf result))
+    ([result [pipeline job :as req]]
+     (when
+         (locking running-jobs
+           (and (nil? (get-in @running-jobs [pipeline job]))
+                (swap! running-jobs assoc-in [pipeline job] true)))
+       (rf result req)))))
+
+(defn remove-finished-jobs
+  [rf]
+  (fn
+    ([] (rf))
+    ([result] (rf result))
+    ([result [pipeline job :as req]]
+     (swap! running-jobs update pipeline dissoc job)
+     (rf result req))))
+
+(defn log-exception
+  [tr]
+  (log/warn tr "Exception during job processing"))
+
+(def job-requests
+  (a/chan))
+
+(def job-results
+  (let [ch (a/chan)]
+    (a/pipeline-blocking
+     (. (Runtime/getRuntime) (availableProcessors))
+     ch
+     (comp remove-running-jobs (map run-job!) remove-finished-jobs)
+     job-requests
+     true
+     log-exception)
+    ch))
+
+;; ## Filesystem watcher (hot-folder mode)
+
+(defn fs-event->job-request
+  [rf]
+  (fn
+    ([] (rf))
+    ([result] (rf result))
+    ([result {:keys [file action] :as evt}]
+     (try
+       (let [^File job-dir (job-dir)]
+         (when (fs/ancestor? job-dir file)
+           (let [[pipeline job] (map str (fs/relativize-path job-dir file))]
+             (when (and pipeline job (fs/file? (pipeline-xpl pipeline)))
+               (let [source-ready (job-status-txt pipeline job "source-ready")]
+                 (when (= source-ready file)
+                   (rf result [pipeline job])))))))
+       ;; Deleted files cannot be augmented with job information
+       (catch NoSuchFileException e)))))
+
+(defstate fs-event-listener
+  :start (let [^File job-dir (fs/make-dir! (job-dir))
+               fs-requests (a/chan 1 fs-event->job-request log-exception)
+               watcher (watch-dir (partial a/>!! fs-requests) job-dir)]
+           (log/infof "Start watching '%s'" job-dir)
+           (a/pipe fs-requests job-requests false)
+           (fn []
+             (log/infof "Stop watching '%s'" job-dir)
+             (close-watcher watcher)
+             (a/close! fs-requests)))
+  :stop (fs-event-listener))
+
+;; ## Cleaning up older jobs
 
 (defstate cleanup-scheduler
   :start
-  (let [{:keys [::job-dir ::cleanup-schedule ::job-max-age]} (mount/args)
+  (let [{:keys [::cleanup-schedule ::job-max-age]} (mount/args)
         next (partial cron/time-to-next-execution cleanup-schedule :millis)
         ch (a/chan)]
     (log/infof "Scheduling cleanup of old jobs (%s)"
                (.asString ^Cron cleanup-schedule))
     (a/go-loop []
       (when-let [_ (a/alt! (a/timeout (next)) :scheduled ch ([v] v))]
-        (log/infof "Cleaning up %s" job-dir)
+        (log/infof "Cleaning up %s" (job-dir))
         (recur)))
     ch)
   :stop
   (a/close! cleanup-scheduler))
 
+(def job-results-pub
+  (a/pub job-results #(subvec % 0 2)))
+
+(defn handle-pipeline-request
+  [{{{:keys [pipeline]} :path} :parameters :as req} respond _]
+  (let [job-id ["test" "82d11012-cf02-4ec0-b3c6-f9fe004de7b0"]
+        result-ch (a/chan)]
+    (a/sub job-results-pub job-id result-ch)
+    (a/go
+      (try
+        (a/alt!
+          result-ch ([result] (respond (response/response {:result result})))
+          (a/timeout 30000) (respond (response/response {:error "Timeout"})))
+        (finally
+          (a/unsub job-results-pub job-id result-ch))))
+    (a/>!! job-requests job-id)))
+
 (defn handle-resource-request
-  [{{{:keys [pipeline job path] :as resource} :path} :parameters :as req}
-   respond _]
+  [{{{:keys [pipeline job path]} :path} :parameters :as req} respond _]
   (let [^File job-dir (job-dir)
         ^File f (fs/file job-dir pipeline job path)
         p (str/join "/" [pipeline job path])]
@@ -118,6 +187,9 @@
 
 (def handlers
   [""
+   ["/:pipeline/"
+    {:handler handle-pipeline-request
+     :parameters {:path (s/keys :req-un [::pipeline])}}]
    ["/:pipeline/:job/*path"
     {:handler handle-resource-request
      :parameters {:path (s/keys :req-un [::pipeline ::job ::path])}
